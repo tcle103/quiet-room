c6445f4adbbd4e3cd84b8512cc8f4e756910db56
tcle103 <146970839+tcle103@users.noreply.github.com>
2023-12-05 17:36:00 -0800

    Styling
    Added the rest of photos into doc and positioned the ones that are in the bedroom

--- js/lab.js
+++ js/lab.js
@@ -28,7 +28,14 @@ window.addEventListener('mousemove', function (e) {
     console.log("im moving!")
 }, false);
 
+
+// item interactivity
+
+// light switch
+var switchSound = document.getElementById("switchSound");
 $("#switch").click(function(){
+    // play light switch sound here
+    switchSound.play();
     console.log("clicked");
     $(".under").toggleClass("clipped");
     if ($(".under").hasClass("clipped")) {
@@ -39,14 +46,53 @@ $("#switch").click(function(){
     }
 })
 
+// alarm clock
+var alarm = document.getElementById("alarm");
+alarm.loop = true;
+setTimeout(function() {
+    // play alarm sound here
+    alarm.play();
+    console.log("ringing");
+}, 5000); // 5000 ms is 5 seconds but we will probably want to make this longer
+
+$("#clock").click(function(){
+    // pause alarm sound here
+    alarm.pause();
+    // reset alarm to start of sound, if this breaks it just remove later
+    alarm.currentTime = 0;
+    console.log("alarm stopped");
+    // comment or remove this part to stop it from ringing again
+    // can also make it only ring a certain amount of times before stop w/ a global var
+    setTimeout(function() {
+        // play alarm sound here
+        alarm.play();
+        console.log("ringing");
+    }, 5000);
+})
+
+// bed 
+var snore = document.getElementById("snore");
+$("#bed").click(function() {
+    // play snore sound here
+    snore.play();
+    console.log("bed clicked");
+})
+
+
+// popups
+
+// overall exit button functionality
+$(".exit_button").click(function() {
+    console.log("popup closed");
+    $("#dark").addClass("hidden");
+    $(this).parent().addClass("hidden");
+})
+
+// phone text popup
 $("#phone-bottom").click(function() {
     console.log("phone bottom clicked");
     $("#dark").removeClass("hidden");
     $("#phone-text-popup").removeClass("hidden");
 })
 
-$(".exit_button").click(function() {
-    console.log("popup closed");
-    $("#dark").addClass("hidden");
-    $(this).parent().addClass("hidden");
-})
+
